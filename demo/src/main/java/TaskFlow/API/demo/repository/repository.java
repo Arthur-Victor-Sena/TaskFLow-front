a7624791c4c34853b48761678d5dd354de10a1d6
Arthur-Victor-Sena <arthurvictorsenarafael@gmail.com>
2026-02-03 15:50:10 -0300

    resolvendo problemas

--- demo/src/main/java/TaskFlow/API/demo/repository/repository.java
+++
@@ -1,8 +0,0 @@
-package TaskFlow.API.demo.repository;
-
-import TaskFlow.API.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface repository extends JpaRepository<User, Long> {
-}
